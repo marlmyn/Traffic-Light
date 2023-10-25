22264cca04825cfe4891793b4c0f25bf1da372fa
Akmaral <e.akmaral024@mail.ru>
2023-10-25 14:58:08 +0600

    Initial Commit

---
+++ Traffic Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Light
+//
+//  Created by Akmaral Ergesh on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
